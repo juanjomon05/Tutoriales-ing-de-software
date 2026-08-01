6621f7a15ab02c63e5c18d2fd743e14037f8feff
juanjitomp2005-hash <124716825+juanjomon05@users.noreply.github.com>
2026-08-01 17:57:53 -0500

    Tutorial 02: agrego de  modelo, datos, rutas y vistas de libros

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from 'express';
+import { books } from '../data/Books.js';
+import { Book } from '../models/Book.js';
 
 export class HomeController {
     static index(req: Request, res: Response): void {
@@ -21,4 +23,15 @@ export class HomeController {
 
         res.render('home/contact', { viewData: viewData });
     }
+
+    static Main_Point(req: Request, res: any) {
+        const viewData: any = {};
+        viewData["books"] = books;
+        res.render('home/books', viewData);
+    }
+    
+    static show(req: any, res: any) {
+        const book = Book.findById(books, parseInt(req.params.id));
+        res.render('home/show', { book: book });
+ }
 }
